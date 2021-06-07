417e992ef5d79a86e8daafaa8bb94fa5dc4989f0
ZeroSG <you@example.com>
2021-06-07 15:47:53 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddstudentComponent } from './addstudent/addstudent.component';
 import { HomeComponent } from './home/home.component';
 import { StudentComponent } from './student/student.component';
 
 const routes: Routes = [
   {path:'student',component:StudentComponent},
   {path:'home',component:HomeComponent},
+  {path:'add',component:AddstudentComponent}
 
 ];
 
